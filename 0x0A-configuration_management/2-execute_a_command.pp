af71ad51541db1d433a598c9d64a52ed416bcf3c
xola-mthembu <xola.mthembu@gmail.com>
2024-04-19 15:08:30 -0700

    2-execute_a_command.pp updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,5 @@
-# File: kill_process.pp
-# This manifest ensures that a process named 'killmenow' is not running
-# by checking for its presence and killing it if found.
-
-exec { 'kill_killmenow_process':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin:/usr/sbin:/bin',
-  onlyif      => "pgrep -f killmenow",
-  provider    => shell,
-  logoutput   => true,
+# Using Puppet, create a manifest that kills a process named killmenow
+exec { 'killmenow':
+  command => 'pkill killmenow || true',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
-
