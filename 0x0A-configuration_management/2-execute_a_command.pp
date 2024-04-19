6ee231c162a82e1dc22afcf31f1301a89a07bd1c
xola-mthembu <xola.mthembu@gmail.com>
2024-04-19 14:30:19 -0700

    Task 2 updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,10 @@
-# This manifest uses the exec resource to kill a process named 'killmenow'
+# File: kill_process.pp
+# This Puppet manifest ensures that any process named 'killmenow' is terminated.
+# It uses the 'pkill' command, which is idempotent in its nature as it will not
+# produce an error if no processes are found that match the name.
 
 exec { 'kill_killmenow_process':
-  command => '/usr/bin/pkill -f killmenow',
-  path    => '/usr/bin:/usr/sbin:/bin',  # Ensure pkill is in the exec path
-  onlyif  => "/usr/bin/pgrep -f killmenow",  # Only run if the process is found
+  command     => 'pkill -f killmenow || :',  # The '-f' flag matches against the complete command line
+  path        => '/usr/bin:/usr/sbin:/bin',
+  refreshonly => true,  # Ensures the command runs only when notified by another resource
 }
