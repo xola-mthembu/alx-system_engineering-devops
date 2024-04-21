9aeebc36ce057dc4c7f240eddb1f780a64a9255c
xola-mthembu <xola.mthembu@gmail.com>
2024-04-21 14:46:03 -0700

    4. Client configuration file (w/ Puppet) fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,24 +1,13 @@
 # Puppet manifest to configure SSH client settings
 
-file_line { 'Declare identity file':
-  path    => '/home/vagrant/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile',
-  replace => true,
-}
-
 file_line { 'Turn off passwd auth':
-  path    => '/home/vagrant/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
-  ensure  => 'present',
-  replace => true,
+  path  => '~/.ssh/config',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
 }
 
-file { '/home/vagrant/.ssh/config':
-  ensure => file,
-  owner  => 'vagrant',
-  group  => 'vagrant',
-  mode   => '0600',
-  require => File_line['Declare identity file', 'Turn off passwd auth'],
+file_line { 'Declare identity file':
+  path  => '~/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
 }
