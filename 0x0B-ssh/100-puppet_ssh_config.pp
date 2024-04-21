8ffc33976c83191dea22409f5b2708989d6002eb
xola-mthembu <xola.mthembu@gmail.com>
2024-04-21 14:41:03 -0700

    4. Client configuration file (w/ Puppet) updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,24 @@
-# Puppet manifest to configure SSH client to use a private key and disable password authentication
+# Puppet manifest to configure SSH client settings
 
 file_line { 'Declare identity file':
-  ensure  => present,
   path    => '/home/vagrant/.ssh/config',
-  line    => '    IdentityFile /home/vagrant/.ssh/school',
-  match   => '^    IdentityFile',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^IdentityFile',
   replace => true,
 }
 
 file_line { 'Turn off passwd auth':
-  ensure  => present,
   path    => '/home/vagrant/.ssh/config',
-  line    => '    PasswordAuthentication no',
-  match   => '^    PasswordAuthentication',
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication',
+  ensure  => 'present',
   replace => true,
+}
+
+file { '/home/vagrant/.ssh/config':
+  ensure => file,
+  owner  => 'vagrant',
+  group  => 'vagrant',
+  mode   => '0600',
+  require => File_line['Declare identity file', 'Turn off passwd auth'],
 }
