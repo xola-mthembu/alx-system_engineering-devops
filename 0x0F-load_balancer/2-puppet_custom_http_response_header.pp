f35fa1ce188928aec101117e1102d52ce19f4228
xola-mthembu <xola.mthembu@gmail.com>
2024-04-29 11:04:45 -0700

    2. Add a custom HTTP header with Puppet updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,34 @@
-# Puppet manifest to configure Nginx with a custom HTTP header
-class nginx_custom_header {
-    package { 'nginx':
-        ensure => installed,
-    }
+# Puppet manifest to automate adding a custom HTTP header in Nginx.
 
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => template("modulename/nginx_default.erb"),
-        require => Package['nginx'],
-        notify  => Service['nginx'],
-    }
-
-    service { 'nginx':
-        ensure => running,
-        enable => true,
-        require => File['/etc/nginx/sites-available/default'],
-    }
+# Ensure the system is updated before installing Nginx.
+exec { 'system_update':
+  provider => shell,
+  command  => 'apt-get -y update',
+  path     => ['/bin', '/usr/bin'],
+  before   => Package['nginx'],
 }
 
-include nginx_custom_header
+# Install the Nginx package if it's not already installed.
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['system_update'],
+}
+
+# Ensure a custom HTTP header is added to the Nginx config.
+file_line { 'add_custom_header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => "\tadd_header X-Served-By \"${facts['networking']['hostname']}\";",
+  match   => '^\\tadd_header X-Served-By',
+  after   => 'server_name _;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Ensure the Nginx service is running and enabled.
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File_line['add_custom_header'],
+}
