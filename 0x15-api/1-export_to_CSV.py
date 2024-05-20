387454b6b4ff1090e6c24f662db2318882ac7999
xola-mthembu <xola.mthembu@gmail.com>
2024-05-20 12:11:24 -0700

    Fix pycodestyle issues in 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,14 +12,15 @@ if __name__ == "__main__":
 
     employee_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    
+
     user = requests.get(url + "users/{}".format(employee_id)).json()
     todos = requests.get(url + "todos", params={"userId": employee_id}).json()
-    
+
     username = user.get("username")
-    
+
     filename = "{}.csv".format(employee_id)
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow([employee_id, username, task.get("completed"), task.get("title")])
+            writer.writerow([employee_id, username,
+                             task.get("completed"), task.get("title")])
