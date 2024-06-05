01ab2af668fe5363db485c875c37c1004d72e583
xola-mthembu <xola.mthembu@gmail.com>
2024-06-05 12:17:30 -0700

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,5 @@
-# Fix Apache 500 error by correcting Apache configuration and restarting service
-exec { 'fix-wordpress':
-  command => 'sed -i "s/AllowOverride None/AllowOverride All/" /etc/apache2/apache2.conf',
+# Correct Apache 500 error by updating permissions for wp-settings.php file
+exec { 'adjust-wordpress':
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
-  onlyif  => 'grep -q "AllowOverride None" /etc/apache2/apache2.conf',
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Exec['fix-wordpress'],
 }
